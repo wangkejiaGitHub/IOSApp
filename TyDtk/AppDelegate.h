ec0999d0be986c5c4cb142d1806d08f4eb61aaa1
wangkejiaGitHub <18838263542@163.com>
2016-03-21 09:17:47 +0800

    test one

---
+++ TyDtk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TyDtk
+//
+//  Created by 天一文化 on 16/3/21.
+//  Copyright © 2016年 天一文化.王可佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
