a57e56a06e3cd685add6014f1f669d696ed2194e
wangkejiaGitHub <18838263542@163.com>
2016-03-21 11:21:46 +0800

    ims

---
+++ TyDtk/ForgetPwdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForgetPwdViewController.h
+//  TyDtk
+//
+//  Created by 天一文化 on 16/3/21.
+//  Copyright © 2016年 天一文化.王可佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForgetPwdViewController : UIViewController
+
+@end
--- TyDtk/LoginViewController.h
+++ TyDtk/LoginViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  TyDtk
 //
 //  Created by 天一文化 on 16/3/21.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LoginViewController : UIViewController
 
 @end
-
